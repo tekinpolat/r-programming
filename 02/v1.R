07fd773701c7f5c480e4209b41470fae22ed7cb2
Tekin POLAT <tekin.polat@ogr.dpu.edu.tr>
2021-10-28 21:11:01 +0300

    example

---
+++ 02/v1.R
@@ -0,0 +1,16 @@
+# what is bigger e^pi or pi^e
+# R pi nasıl tanımlayacaz
+
+
+#print
+#paste 
+#paste0
+#cat      
+
+
+
+#aritmetik operatörler 
+# + - * / % ^
+
+x <- 5 ^ 3
+print(x)
---
+++ 02/v1.cpp
@@ -0,0 +1,10 @@
+int main(){
+    int x = 5;
+    int y = 3;
+
+    cout >> x % y;
+}
+
+void yazdir(int x, int y, int z){
+    
+}
